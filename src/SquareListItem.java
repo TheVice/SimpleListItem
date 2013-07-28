05f3a0d73420e4da8007928a32861f90f1970aac
b0noI <viacheslav@b0noi.com>
2013-07-28 19:30:23 +0300

    First fest comments

--- src/SquareListItem.java
+++ src/SquareListItem.java
@@ -1,4 +1,3 @@
-
 public class SquareListItem {
 
     private static int mSID;
@@ -7,13 +6,16 @@ public class SquareListItem {
     private SquareListItem mNext;
 
     public SquareListItem() {
-
+        // what this counter is for? Just instance counting?
         mID = mSID++;
     }
 
     public SquareListItem setSquare(Square aSquare) {
 
         aSquare.validate();
+        // why we need copy constructor here? 
+        // i thinsk is better to setup direct mSquare = aSquare;
+        // PS: in Java style this string should be simething like this: this.square = square;  
         mSquare = new Square(aSquare);
         mNext = new SquareListItem();
         return mNext;
