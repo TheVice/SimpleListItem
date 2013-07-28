6258157503a86392f37acd34d707141f3ac37049
b0noI <viacheslav@b0noi.com>
2013-07-28 19:34:41 +0300

    First comments

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,3 @@
-
 public class Point {
 
     private float mX, mY;
@@ -7,6 +6,15 @@ public class Point {
 
     }
 
+    // Using copy constructo in all classes is not a good thing. 
+    // This is Ok in C++ (becouse C++ create copy constructor in auto mode) but in Java usualy try not to implenet it everywere
+    // i will recheck logic but not today =(, PS: i this tommorrow morning
+    // and in this case better to call another constructor:
+    /*
+        public Point(Point aPoint) {
+            this(aPoint.getX(), aPoint.getY());
+        }   
+    */
     public Point(Point aPoint) {
 
         set(aPoint.getX(), aPoint.getY());
